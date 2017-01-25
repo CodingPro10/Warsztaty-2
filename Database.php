4734a90fdebb2d96182bca6ce757e14309137f4c
Mariusz Kwieciński <m.kwiecinski@wp.pl>
2017-01-25 14:30:53 +0100

    Zadania wykonane na zajęciach w dn. 21-22.01.2017r

---
+++ Database.php
@@ -0,0 +1,14 @@
+<?php
+
+class Database {
+    
+    private $host = "loclahost";
+    private $user = "root";
+    private $password = "coderslab";
+    private $db = "Twitter";
+            
+    public function __construct() {
+        $this->connection = new mysqli 
+    }
+}
+
---
+++ User.php
@@ -0,0 +1,15 @@
+<?php
+require 'Database.php';
+
+class User extends Database {
+
+    private $id;
+    private $email;
+    private $username;
+    private $hashed_password;
+
+    public function __construct($email, $username, ) {
+        
+    }b
+    
+}
